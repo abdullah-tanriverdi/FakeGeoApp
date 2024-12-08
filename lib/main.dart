b9fc540e4ddc74d0eb36789bde633a7ad8997d38
abdullah-tanriverdi <tanriverdi.firatuni@gmail.com>
2024-12-08 23:49:16 +0300

    Ana Renk Yeşil Olarak Ayarlandı

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
